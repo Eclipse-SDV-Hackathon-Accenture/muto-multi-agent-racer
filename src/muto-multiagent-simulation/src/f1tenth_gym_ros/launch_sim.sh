3016e119d014ab686cca852bb502a7bf09a61be2
İbrahim Sel <ibrahim.sel@eteration.com>
2023-11-29 16:04:17 +0300

    removed unnecessary files

--- src/muto-multiagent-simulation/src/f1tenth_gym_ros/launch_sim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This file is for docker container
-export ROS_DOMAIN_ID=42
-source /opt/ros/foxy/setup.bash
-source /sim_ws/install/setup.bash
-ros2 launch f1tenth_gym_ros gym_bridge_launch.py
